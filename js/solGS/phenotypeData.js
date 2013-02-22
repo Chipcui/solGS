adfbfdc24dc7b70530901253837e41e6327301bf
isaak <iyt2@cornell.edu>
2013-02-22 14:20:35 +0100

    alert user plotting failure..

--- js/solGS/phenotypeData.js
+++ js/solGS/phenotypeData.js
@@ -17,8 +17,8 @@ jQuery(window).load( function() {
       
         var params = 'pop_id' + '=' + popId + '&' + 'trait_id' + '=' + traitId;
         var action = '/phenotype/graph';
-
-        var phenoDataRenderer = function(url, plot, options) {
+     
+        var phenoDataRenderer = function() {
             var graphArray = [];
             graphArray[0] = [];
            
@@ -29,75 +29,83 @@ jQuery(window).load( function() {
                         data: params,
                         success: function(data) {
                         traitData = data.trait_data;
-                        
-                        for (var i=0; i < traitData.length; i++) {
-                             var xd = traitData[i][0];
-                             xd = xd.toString();
-                             var yd = traitData[i][1];
-                             yd = yd.replace(/\s/, '');
-                             yd = Number(yd);
+                        var state = data.status;
+                     
+                        if (state == 'success') {
+                            for (var i=0; i < traitData.length; i++) {
+                                var xd = traitData[i][0];
+                                xd = xd.toString();
+                                var yd = traitData[i][1];
+                                yd = yd.replace(/\s/, '');
+                                yd = Number(yd);
 
-                             graphArray[0][i]    = [];
-                             graphArray[0][i][0] = xd;
-                             graphArray[0][i][1] = yd;                                 
+                                graphArray[0][i]    = [];
+                                graphArray[0][i][0] = xd;
+                                graphArray[0][i][1] = yd;                                 
+                            }
                         }
                     }
                 });
-       
+            
             return graphArray;
             
         };
-    
-  var graph  = jQuery.jqplot('phenoHistogram', {
-    title: "Phenotype data",
-    dataRenderer: phenoDataRenderer,
-    seriesDefaults: {
-              showLine: false,
-              shadow: false,
-              markerOptions: {
-                  size: 6,
-                  shadow: false,                  
-              },
-    },
-    grid: { 
-              drawGridLines: false,
-              gridLineColor: '#ffffff',
-              background: '#ffffff',
-              shadow: false,
-    },
-    highlighter: {
-              show: true,
-              sizeAdjust: 10,
-              tooltipAxes: 'xy',
-              useXTickMarks: true,
-              useYTickMarks: true,
-         
-      },
-    cursor: {
-              show: true,
-              useAxesFormatters: true,
-              showTooltip: false,
-    },
-    axes: {
-              xaxis: {
-                  renderer:jQuery.jqplot.CategoryAxisRenderer,
-                  tickRenderer: jQuery.jqplot.CanvasAxisTickRenderer,
-                  tickOptions: {
-                      angle: -90, 
-                      fontSize:5,
-                  },
-              },
-              yaxis: {
-                  tickOptions: { 
-                      fontSize: 9,
-                  },
-              
-              },
-    }
-
-  });
+  
+        var phenoData = phenoDataRenderer();          
+        if (phenoData == 'undefined') {
+            var message = 'There is no phenotype data to plot. Please report this problem';  
+            jQuery('#phenoHistogram').append(message).show();
+        } else {        
+            var graph = jQuery.jqplot('phenoHistogram', {
+                    title: "Phenotype data",
+                    dataRenderer: phenoDataRenderer,
+                    seriesDefaults: {
+                        showLine: false,
+                        shadow: false,
+                        markerOptions: {
+                            size: 6,
+                            shadow: false,                  
+                        },
+                    },
+                    grid: { 
+                        drawGridLines: false,
+                        gridLineColor: '#ffffff',
+                        background: '#ffffff',
+                        shadow: false,
+                    },
+                    highlighter: {
+                        show: true,
+                        sizeAdjust: 10,
+                        tooltipAxes: 'xy',
+                        useXTickMarks: true,
+                        useYTickMarks: true,        
+                    },
+                    cursor: {
+                        show: true,
+                        useAxesFormatters: true,
+                        showTooltip: false,
+                    },
+                    axes: {
+                        xaxis: {
+                            renderer:jQuery.jqplot.CategoryAxisRenderer,
+                            tickRenderer: jQuery.jqplot.CanvasAxisTickRenderer,
+                            tickOptions: {
+                                angle: -90, 
+                                fontSize:5,
+                            },
+                        },
+                        yaxis: {
+                            tickOptions: { 
+                                fontSize: 9,
+                            },              
+                        },
+                    }
+                });
+       }
+     
+////
 });
-
+////
 
 
 
